0dda6866b3613dcf6cef0709b07527a56a22bdf6
František Gič <mcfreddie777@gmail.com>
2022-01-02 19:40:17 +0100

    Use XGBoost algorithm

--- src/_/constants.py
+++ src/_/constants.py
@@ -4,8 +4,6 @@ from pathlib import Path
 DATA_DIR= (Path(__file__) / '../../../data').resolve()
 TRAIN = DATA_DIR / 'train.csv'
 TEST = DATA_DIR / 'test.csv'
-TRAIN_DROPPED = DATA_DIR / 'train-dropped.parquet'
-TEST_DROPPED = DATA_DIR / 'test-dropped.parquet'
-TRAIN_PREPROCESSED = DATA_DIR / 'train-preprocessed.parquet'
-TEST_PREPROCESSED = DATA_DIR / 'test-preprocessed.parquet'
-SCHEMA = DATA_DIR / 'schema.json'
+DROPPED = DATA_DIR / 'dropped.parquet'
+PREPROCESSED = DATA_DIR / 'preprocessed.parquet'
+METADATA = DATA_DIR / 'item_metadata.csv'
--- src/_/functions.py
+++ src/_/functions.py
@@ -1,5 +1,8 @@
+import re
 import pandas as pd
+import numpy as np
 from functools import partial
+from tqdm import tqdm
 
 def drop_single_action_users(df):
     user_id_count = df.user_id.value_counts().reset_index(name="count").rename(columns={'index': 'user_id'})
@@ -40,9 +43,12 @@ def drop(path):
     return df
 
 
-def explode_multivalue_attributes(df,columns):
+def explode_multivalue_attributes(df,columns,cast=None):
     for col in columns:
-        df[col] = df[col].apply(lambda x: x.split('|') if x != None else x)
+        if cast is None:
+            df[col] = df[col].apply(lambda x: x.split('|') if x != None else x)
+        else:
+            df[col] = df[col].apply(lambda x: list(map(cast,x.split('|'))) if x != None else x)
         
     return df
 
@@ -55,16 +61,66 @@ def encode_categorical(df,columns):
 
 def one_hot_encode(df,columns):
     return pd.get_dummies(data=df, columns=columns)
+
+def impression_index(df):
+    def _get_imp_index(reference,impressions):
+        if impressions is not None:
+            return next((index for index,impr in enumerate(impressions) if impr == reference),np.nan)
+        return np.nan
     
-    
-def remove_step_column(df):
-    df.sort_values(['session_id','step'])
-    # no longer needed column
-    del df['step']
+    df['impression_index'] = df.apply(lambda x: _get_imp_index(x['reference'], x['impressions']), axis=1)
 
     return df
+    
+    
+def calculate_price(df):
+    df['price'] = df.apply(lambda x: x['prices'][int(x['impression_index'])] if ~np.isnan(x['impression_index']) else np.nan, axis=1)
+    df['mean_price'] = df['prices'].apply(lambda x: np.mean(x) if x is not None else np.nan)
+    
+    return df
 
-def preprocess(path,backup_path):    
+def parse_stars(string):
+    result = re.search(r'^(\d) star$',string, flags=re.IGNORECASE)
+    if result != None:
+        return int(result.group(1))
+    
+def parse_ratings(df):
+    rating_map = {
+    'Satisfactory Rating': 7.0,
+    'Good Rating': 7.5,
+    'Very Good Rating': 8.0,
+    'Excellent Rating': 8.5
+    }
+    
+    for rating_key in rating_map:
+        df.loc[df['properties'].apply(lambda x: rating_key in x),'rating'] = rating_map[rating_key]    
+        
+    return df
+
+
+def merge_meta(df,meta):
+    for index,_ in enumerate(tqdm(df.itertuples(), desc="Iterating rows", total=df.shape[0])):
+        row = df.iloc[int(index)]
+        try:
+            item = meta.loc[int(row['reference'])]
+            row['stars'] = item['stars']
+            row['rating'] = item['rating']
+        except:
+            row['stars'] = np.nan
+            row['rating'] = np.nan
+             
+    return df
+    
+def preprocess_meta(df,meta):
+    meta.set_index('item_id',inplace=True)
+    meta['properties'] = meta['properties'].apply(lambda x: x.split('|'))
+    meta['stars'] = meta['properties'].apply(lambda props: next((i for i in list(map(parse_stars, props)) if i is not None), np.nan))
+    meta = parse_ratings(meta)
+    df = merge_meta(df,meta)
+    
+    return df    
+
+def preprocess(path,backup_path,meta_path):    
 
     #check if data exists
     if not path.exists():
@@ -72,16 +128,21 @@ def preprocess(path,backup_path):
         df.to_parquet(path, index=False)
     else:
         df = pd.read_parquet(path)
-
+        
+    meta = pd.read_csv(meta_path)
     
     functions = [
-        partial(explode_multivalue_attributes,columns=['impressions','prices','current_filters']),
-        partial(encode_categorical, columns=['user_id','session_id', 'action_type', 'platform', 'city']),
+        partial(explode_multivalue_attributes,columns=['impressions','current_filters']),
+        partial(explode_multivalue_attributes,columns=['prices'], cast=int),
+        impression_index,
+        calculate_price,
+        partial(encode_categorical, columns=['session_id', 'action_type', 'platform', 'city']),
         partial(one_hot_encode, columns=['device']),
-        remove_step_column
+        partial(preprocess_meta,meta=meta),
     ];
     
     for fun in functions:
+        print(f"Running {fun.__name__ if hasattr(fun,'__name__') else fun.func.__name__}...")
         df = fun(df);
     
     return df
