47cc498679108c749eac6940c1ac0b954967a136
František Gič <mcfreddie777@gmail.com>
2023-05-08 11:28:20 +0200

    Enhance popularity model by distinct users interactions with item

--- src/_helpers/constants.py
+++ src/_helpers/constants.py
@@ -14,3 +14,11 @@ GROUND_TRUTH = RAW_DIR / 'ground_truth.csv'
 DROPPED = DROPPED_DIR / 'train.parquet'
 def DROPPED_SUBSET(subset_n):
     return DROPPED.with_name(f'train_subset_{subset_n}.parquet')
+
+
+# Column names
+GT_COLS = ["user_id", "session_id", "timestamp", "step"]
+ITEM_REFERENCE_ACTION_TYPE_COLS = [
+    'clickout item', 'interaction item deals', 'interaction item image',
+    'interaction item info', 'interaction item rating', 'search for item'
+]
--- src/models/model_popular.py
+++ src/models/model_popular.py
@@ -1,3 +1,4 @@
+from _helpers import constants
 from _helpers import functions as hf
 
 
@@ -13,19 +14,26 @@ class ModelPopular():
     def fit(self, df):
         """Get number of clicks that each item received in the df."""
         
-        features = ['user_id', 'session_id', 'timestamp', 'step', 'action_type', 'reference']
-        df_cols = df.copy().loc[:, features]
-        
-        df_item_clicks = (
+        df_cols = df.copy().loc[df["action_type"].isin(constants.ITEM_REFERENCE_ACTION_TYPE_COLS), :]
+
+        # Get number of user interactions with specific item
+        df_user_item_interactions = (
             df_cols
-            .loc[df_cols["action_type"] == "clickout item", :]
-            .groupby("reference")
+            .groupby(['user_id', 'reference'])
             .size()
-            .reset_index(name="n_clicks")
-            .rename(columns={"reference": "item"})
+            .reset_index(name='user_item_interactions')
         )
         
-        self.df_pop = df_item_clicks
+        # Get global number of interactions with the specific item
+        df_item_interactions = (
+            df_cols
+            .groupby('reference')
+            ['user_id']
+            .nunique()
+            .reset_index(name="item_interactions")
+        )
+        
+        self.df_user_item_pop = df_user_item_interactions.merge(df_item_interactions,on="reference").rename(columns={'reference':'item'})
     
     def predict(self, df):
         features = ['user_id', 'session_id', 'timestamp', 'step', 'action_type', 'reference', 'impressions']
@@ -33,26 +41,30 @@ class ModelPopular():
         
         df_target = hf.get_target_rows(df_cols.copy())
         
+        # Explode the impressions into separate rows
         df_impressions = (
             hf.explode(df_target, "impressions")
             .rename(columns={"impressions": "impressed_item"})
         )
         
+        # Merge with preprocessed dataset from fit function
         df_impressions = (
             df_impressions
             .merge(
-                self.df_pop,
-                left_on="impressed_item",
-                right_on="item",
+                self.df_user_item_pop,
+                left_on=['user_id',"impressed_item"],
+                right_on=['user_id',"item"],
                 how="left"
             )
         )
         
+        # Sort impressions by user_item interactions and global interactions
         df_rec = (
             df_impressions
             .sort_values(
-                by=["user_id", "session_id", "timestamp", "step", 'n_clicks'],
-                ascending=[True, True, True, True, False]
+                by=["user_id", "session_id", "timestamp", "step",'user_item_interactions','item_interactions'],
+                ascending=[True, True, True, True, False,False],
+                na_position='last'
             )
             .groupby(["user_id", "session_id", "timestamp", "step"])["impressed_item"]
             .apply(lambda x: ' '.join(x))
