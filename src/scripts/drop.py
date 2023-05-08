5bda067b5dd0b8f229d2c50afaca343f6d3f1da5
František Gič <mcfreddie777@gmail.com>
2023-05-08 21:30:07 +0200

    Refactor dropping script to drop test and ground_truth file as well

--- src/scripts/drop.py
+++ src/scripts/drop.py
@@ -1,18 +1,35 @@
 import pandas as pd
 
 from .._helpers import constants
-from .._helpers.drop import drop
+from .._helpers.drop import drop, __exclude_user_session_pairs
 
 def main():
     """
     This function creates a parquet file which cleans up data
     """
     
+    # Drop train dataset
+    print(f"Reading {constants.TRAIN}...")
     df_train = pd.read_csv(constants.TRAIN)
+    df_train, deleted_user_session_pairs = drop(df_train)
+    df_train.to_parquet(constants.DROPPED_TRAIN, index=False)
+    print(f"Output saved to {constants.DROPPED_TRAIN}.")
     
-    df_train = drop(df_train)
+    # Remove dropped sessions from test dataset
+    print(f"Reading {constants.TEST}...")
+    df_test = pd.read_csv(constants.TEST)
+    df_test = __exclude_user_session_pairs(df_test, deleted_user_session_pairs)
+    print(f"Removed {len(deleted_user_session_pairs)} from test dataset.")
+    df_test.to_parquet(constants.DROPPED_TEST, index=False)
+    print(f"Output saved to {constants.DROPPED_TEST}.")
     
-    df_train.to_parquet(constants.DROPPED, index=False)
+    # Remove dropped sessions from ground_truth dataset
+    print(f"Reading {constants.GROUND_TRUTH}...")
+    df_gt = pd.read_csv(constants.GROUND_TRUTH)
+    df_gt = __exclude_user_session_pairs(df_gt, deleted_user_session_pairs)
+    print(f"Removed {len(deleted_user_session_pairs)} from ground_truth.")
+    df_gt.to_parquet(constants.DROPPED_GROUND_TRUTH, index=False)
+    print(f"Output saved to {constants.DROPPED_GROUND_TRUTH}.")
 
 
 if __name__ == "__main__":
