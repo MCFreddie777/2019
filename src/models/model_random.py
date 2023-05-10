0e55c5c50ef24ff11d82b99c1374540b079f2c81
František Gič <mcfreddie777@gmail.com>
2023-05-10 21:35:52 +0200

    Query default random seed when none provided

--- src/models/model_random.py
+++ src/models/model_random.py
@@ -2,24 +2,25 @@ import random
 
 from _helpers import functions as f
 
+
 class ModelRandom():
     """This model randomly sorts the impressions list."""
     params = {
-        'seed': 123
+        'seed': 1065
     }
     
-    def update(self,params):
-        self.params = params
+    def update(self, params):
+        if ('seed' in params):
+            self.params['seed'] = params['seed']
     
     def fit(self, _):
         pass
     
     def predict(self, df):
-        
         df_target = f.get_target_rows(df.copy())
         
         random.seed(self.params['seed'])
-    
+        
         df_target.loc[:, "item_recommendations"] = (
             df_target
             .loc[:, "impressions"].str.split("|")
@@ -29,5 +30,5 @@ class ModelRandom():
         
         cols_rec = ["user_id", "session_id", "timestamp", "step", "item_recommendations"]
         df_rec = df_target.loc[:, cols_rec]
-
+        
         return df_rec
