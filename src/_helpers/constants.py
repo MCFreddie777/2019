347294119c08a3aa99498fd852d7068c37778b64
František Gič <mcfreddie777@gmail.com>
2023-05-11 15:56:18 +0200

    Remvoe subsetting

--- src/_helpers/constants.py
+++ src/_helpers/constants.py
@@ -3,30 +3,23 @@ from dotenv import load_dotenv
 
 from .functions import get_env
 
-load_dotenv(override=True) # Load env variables from .env file
-
-# Special
-SUBSET=get_env('SUBSET', None)
+load_dotenv(override=True)  # Load env variables from .env file
 
 # Directories
-DATA_DIR= (Path(__file__) / '../../../data').resolve()
-INPUT_DIR= DATA_DIR / 'input'
+DATA_DIR = (Path(__file__) / '../../../data').resolve()
+INPUT_DIR = DATA_DIR / 'input'
 OUTPUT_DIR = DATA_DIR / 'output'
 DROPPED_DIR = DATA_DIR / 'dropped'
 PREPROCESSED_DIR = DATA_DIR / 'preprocessed'
 
 # Files
 TRAIN = INPUT_DIR / get_env('TRAIN', 'train.csv')
-TEST = INPUT_DIR /  get_env('TEST', 'test.csv')
+TEST = INPUT_DIR / get_env('TEST', 'test.csv')
 METADATA = INPUT_DIR / get_env('METADATA', 'item_metadata.csv')
 GROUND_TRUTH = INPUT_DIR / get_env('GROUND_TRUTH', 'test_ground_truth.csv')
 
 DROPPED_TRAIN = DROPPED_DIR / 'train.parquet'
 DROPPED_TEST = DROPPED_DIR / 'test.parquet'
-
-
-def DROPPED_SUBSET(subset_n, type):
-    return DROPPED_DIR / f'{type}_subset_{subset_n}.parquet'
 
 
 def PREPROCESSED(n, type):
