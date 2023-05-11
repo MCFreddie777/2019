a3416ed629bc900ee91b4f813d19ec696d855869
František Gič <mcfreddie777@gmail.com>
2023-05-12 00:34:15 +0200

    Train MLP model on chunks

--- src/models/model_mlp.py
+++ src/models/model_mlp.py
@@ -2,6 +2,7 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import randint
+import pandas as pd
 
 from _helpers import functions as hf
 
@@ -12,23 +13,20 @@ class ModelMLP():
     """
     params = {}
     
+    def __transform_for_fitting(self, filename):
+        df = pd.read_parquet(filename)
+        
+        df.loc[:, "is_clicked"] = (df["referenced_item"] == df["impressed_item"]).astype(int)
+        
+        X = df[self.params['features']]
+        y = df.is_clicked
+        
+        return X, y
+    
     def update(self, params):
         self.params = params
     
     def fit(self, *args, **kwargs):
-        df_impressions = hf.load_preprocessed_dataset('train')
-        
-        df_impressions.loc[:, "is_clicked"] = (
-                df_impressions["referenced_item"] == df_impressions["impressed_item"]
-        ).astype(int)
-        
-        X = df_impressions[self.params['features']]
-        y = df_impressions.is_clicked
-        
-        # Perform feature scaling
-        self.scaler = StandardScaler()
-        X = self.scaler.fit_transform(X)
-        
         # Define the parameter grid for randomized search
         param_grid = {
             'hidden_layer_sizes': randint(64, 256),
@@ -37,14 +35,29 @@ class ModelMLP():
             'alpha': [0.0001, 0.001, 0.01]
         }
         
-        # Train the model
-        model = MLPRegressor(random_state=42)
+        train_chunks = hf.get_preprocessed_dataset_chunks('train')
+        
+        # We will optimalize hypterparameters on first chunk
+        X, y = self.__transform_for_fitting(train_chunks[0])
+        
+        # Define model
+        model = MLPRegressor(random_state=42, verbose=True)
         
         # Perform randomized search
-        randomized_search = RandomizedSearchCV(estimator=model, param_distributions=param_grid, n_iter=10, cv=3, verbose=True)
+        randomized_search = RandomizedSearchCV(
+            estimator=model,
+            param_distributions=param_grid,
+            n_iter=5,
+            cv=3,
+            verbose=True
+        )
         randomized_search.fit(X, y)
         
         self.model = randomized_search.best_estimator_
+        
+        for i, chunk_filename in enumerate(train_chunks):
+            X, y = self.__transform_for_fitting(chunk_filename)
+            self.model.partial_fit(X, y)
     
     def predict(self, *args, **kwargs):
         df_impressions = hf.load_preprocessed_dataset('test')
@@ -52,9 +65,6 @@ class ModelMLP():
         df_impressions = df_impressions[df_impressions.referenced_item.isna()]
         
         X = df_impressions[self.params['features']]
-        
-        # Perform feature scaling using the fitted StandardScaler from the training data
-        X = self.scaler.transform(X)
         
         # Make predictions using the trained model
         df_impressions.loc[:, "click_probability"] = (self.model.predict(X))
